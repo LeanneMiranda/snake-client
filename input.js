19cb22cf1737b420439ddb1633fff879435a45a4
LeanneMiranda <leannemiranda@gmail.com>
2022-10-31 20:28:39 -0700

    created 'input' module

---
+++ input.js
@@ -0,0 +1,20 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  process.stdin.on('data', (key) => {
+    process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  setupInput();
+};
+
+module.exports = { setupInput };
