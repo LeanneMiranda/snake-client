29683276ec88eab3afe31aa5e652fecdb1ad0d78
LeanneMiranda <leannemiranda@gmail.com>
2022-10-31 20:29:33 -0700

    moved 'setupInput' into new module 'input'

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-// setup interface to handle user input from stdin
+console.log("Connecting ...");
+connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  process.stdin.on('data', (key) => {
-    process.stdout.write('.');
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-  setupInput();
-};
-
-  console.log("Connecting ...");
-  connect();
+setupInput();
