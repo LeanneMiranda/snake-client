78661aa9f29c326ce1b35262692953dcd63d8517
LeanneMiranda <leannemiranda@gmail.com>
2022-10-31 21:40:08 -0700

    added 'wasd' commands

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,14 +10,23 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function () {
-  process.stdin.on('data', (key) => {
-    process.stdout.write('.');
+const handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
     }
-  });
-  setupInput();
-};
+    if (key === 'w') {
+      connection.write("Move: up")
+      // console.log("Move: up");
+    }
+    if (key === 'a') {
+      connection.write("Move: left")
+    }
+    if (key === 's') {
+      connection.write("Move: down")
+    }
+    if (key === 'd') {
+      connection.write("Move: right")
+    }
+  };
 
 module.exports = { setupInput };
