886888f9988fac814984418a6396edeee2213aa4
LeanneMiranda <leannemiranda@gmail.com>
2022-10-31 19:56:54 -0700

    added new function to handle user input and stdin

--- play.js
+++ play.js
@@ -1,4 +1,25 @@
-const {connect} = require('./client');
+const { connect } = require('./client');
 
-console.log("Connecting ...");
-connect();
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  process.stdin.on('data', (key) => {
+    process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  setupInput();
+};
+
+  console.log("Connecting ...");
+  connect();
